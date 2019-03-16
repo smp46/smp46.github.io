ca307cf2d50234b20cd1303b0f7004da195f54cc
smp46 <sammylonghead@gmail.com>
2019-03-16 11:47:04 +1000

    fix

--- js.js
+++ js.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("nav");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
