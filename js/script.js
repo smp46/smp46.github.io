e07faa23b01b6732138f72e9e933cf1c97fdba0c
smp46 <27676057+smp46@users.noreply.github.com>
2024-04-26 11:02:14 +1000

    Update.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function showGUI() {
+    document.querySelector('.media-frame').src = 'images/MazeGameDemo.gif';
+}
+
+function showGUINA() {
+    document.querySelector('.media-frame').src = 'images/MazeGameNoAssets.png';
+}
+
+function showCLI() {
+    document.querySelector('.media-frame').src = 'images/MazeGameCLI.png';
+}
