04ad6415b6cd7b539f7fdcfc94fc6efaa8c964e7
smp46 <27676057+smp46@users.noreply.github.com>
2024-12-01 10:49:25 +1000

    Removed basepaths and asset prefix.

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/smp46.me/' : '',
-  basePath: process.env.DEPLOYED_GITHUB_PATH || isProd ? '/smp46.me' : '',
   output: 'export',
   trailingSlash: true,
 };
