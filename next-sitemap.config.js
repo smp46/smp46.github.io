36033be5b6ac69697ac793e69ca143a612ea2c90
smp46 <27676057+smp46@users.noreply.github.com>
2025-04-14 11:35:47 +1000

    Rolled robots.txt back, disabled auto-generation by next-sitemap module

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,5 @@
 // Sitemap configuration
 module.exports = {
-  siteUrl: 'https://smp46.me', // Replace with your domain
-  generateRobotsTxt: true,    // Generate robots.txt
+  siteUrl: 'https://smp46.me',
   generateIndexSitemap: false,
-  // additionalPaths: (config) => {
-  //   // Generate dynamic paths for the Markdown files
-  //   const projectPaths = getProjectSlugs().map((slug) => ({
-  //     loc: `${config.siteUrl}${slug}`, // Properly format URLs for dynamic projects
-  //     lastmod: new Date().toISOString(),
-  //     changefreq: 'daily',
-  //     priority: 0.7,
-  //   }));
-  //
-  //   return projectPaths;
-  // },
 };
