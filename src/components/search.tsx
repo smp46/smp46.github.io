3a6fbd548e54b646d04472581f9470cc6671ba30
smp46 <me@smp46.me>
2025-05-13 16:28:07 +1000

    Attempt again to change mark colour in search results

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -221,18 +221,18 @@ function SearchResultItem({
       onMouseEnter={onMouseEnter}
     >
       <h4 className="font-medium">{loadedData.meta.title}</h4>
-        <style jsx>{`
-          .search-results-container :global(mark) {
+        <style jsx global>{`
+            p mark {
             background-color: #d3d3d3;
             color: #000;
             padding: 0 2px;
             border-radius: 2px;
-          }
+            }
         `}</style>
-      <p
-        className="text-sm text-gray-600"
-        dangerouslySetInnerHTML={{ __html: loadedData.excerpt }}
-      ></p>
+        <p
+            className="text-sm text-gray-600"
+            dangerouslySetInnerHTML={{ __html: loadedData.excerpt }}
+        ></p>
     </div>
   );
 }
