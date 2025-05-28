9ff80511dce7df8c8a94f1c7e98ac99faaa39ba9
smp46 <me@smp46.me>
2025-05-28 10:33:00 +1000

    Moved asset folder to public dir, renamed all mentions to match.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,7 +61,7 @@ export default function Welcome() {
             >
               <Image
                 ref={leftGifRef}
-                src="https://cdn.statically.io/gh/smp46/smp46.github.io/nextjs/assets/me3.webp"
+                src="https://cdn.statically.io/gh/smp46/smp46.github.io/nextjs/public/assets/me3.webp"
                 alt="Left GIF animation"
                 unoptimized={false}
                 fill
@@ -69,7 +69,7 @@ export default function Welcome() {
               />
               <Image
                 ref={rightGifRef}
-                src="https://cdn.statically.io/gh/smp46/smp46.github.io/nextjs/assets/snake3.webp"
+                src="https://cdn.statically.io/gh/smp46/smp46.github.io/nextjs/public/assets/snake3.webp"
                 unoptimized={false}
                 alt="Right GIF animation"
                 fill
@@ -77,7 +77,7 @@ export default function Welcome() {
               />
               <Image
                 ref={finalImageRef}
-                src="https://cdn.statically.io/gh/smp46/smp46.github.io/nextjs/assets/pp.webp?q=85"
+                src="https://cdn.statically.io/gh/smp46/smp46.github.io/nextjs/public/assets/pp.webp?q=85"
                 alt="Final Image"
                 fill
                 style={{ position: 'absolute', top: 0, left: 0, opacity: 0 }}
